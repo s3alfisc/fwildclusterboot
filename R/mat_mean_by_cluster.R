4b658ea7cc4167983266625afdfcfb7f72b265ab
Alexander Fischer <alexander-fischer1801@t-online.de>
2020-04-15 16:53:54 +0200

    Add an example based on a "real" data set to readme.

--- R/mat_mean_by_cluster.R
+++ R/mat_mean_by_cluster.R
@@ -1,5 +1,9 @@
 mat_mean_by_cluster <- function(prod, clustid){
   
+  
+  if(!is.matrix(prod)){stop("Prod is not an object of type matrix.")}
+  if(!is.vector(clustid)){stop("clustid is not an object of type vector.")}
+  
   mat_prep <- data.table::data.table(prod = prod , clustid = clustid) 
   mat_u <- as.matrix(mat_prep[, lapply(.SD, sum), by = "clustid"][
     , clustid := NULL])
