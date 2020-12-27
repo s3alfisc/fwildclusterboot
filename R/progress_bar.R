e3da84db4d87bf8c8e1fdb691e53a672c587cdbb
Alexander Fischer <alexander-fischer1801@t-online.de>
2020-12-27 14:50:34 +0100

    delete

---
+++ R/progress_bar.R
@@ -0,0 +1,14 @@
+# progress_bar <- function(i, n){
+#   width <- options()$width
+#   cat(paste0(rep('.', i / n * width), collapse = ''))
+#   #Sys.sleep(.05)
+#   if (ii == n) cat('.')
+#   else cat('\014')
+# }
+
+
+
+# n <- 26
+#  for (ii in 1:n) {
+#    progress_bar(ii, n = n)
+#  }
