942c585927586b0a6b302a1ee87ff2d1c905dcb7
Alexander Fischer <alexander-fischer1801@t-online.de>
2020-12-28 16:38:12 +0100

    comment out dgp

--- R/boottest_lm.R
+++ R/boottest_lm.R
@@ -28,33 +28,33 @@ boottest.lm <- function(object,
   #print(B)
   
    #execute all functions in fwildclusterboot 
-                          # setwd("C:/Users/alexa/Dropbox/fwildclusterboot/R")
-                          #  file.sources = list.files(pattern="*.R")
-                          # sapply(file.sources, source, .GlobalEnv)
-                          #   #set.seed(6)
-                          # voters <-
-                          #   create_data_2(
-                          #     N = 10000,
-                          #     N_G1 = 20,
-                          #     icc1 = 0.01,
-                          #     N_G2 = 10,
-                          #     icc2 = 0.01,
-                          #     numb_fe1 = 10,
-                          #    numb_fe2 = 10,
-                          #     seed = 12345
-                          #   )                   #   # create a missing variable in group_id1
-                          #   voters[1, group_id1 := NA]
-                          #   voters[2, proposition_vote := NA]
-                          #   object <- lm(proposition_vote ~ treatment + ideology1 + log_income + Q1_immigration , weights = NULL, data = voters)
-                          #   clustid <- c("group_id1") 
-                          #   param <- "treatment"
-                          # beta0 = 0
-                          #   alpha = 0.05
-                          #   B = 100000
-                          #   weights = NULL
-                          #   conf_int = NULL 
-                          #   debug = FALSE
-                          #   seed = 1234
+                           # setwd("C:/Users/alexa/Dropbox/fwildclusterboot/R")
+                           #  file.sources = list.files(pattern="*.R")
+                           # sapply(file.sources, source, .GlobalEnv)
+                           #   #set.seed(6)
+                           # voters <-
+                           #   create_data_2(
+                           #     N = 10000,
+                           #     N_G1 = 20,
+                           #     icc1 = 0.01,
+                           #     N_G2 = 10,
+                           #     icc2 = 0.01,
+                           #     numb_fe1 = 10,
+                           #    numb_fe2 = 10,
+                           #     seed = 12345
+                           #   )                   #   # create a missing variable in group_id1
+                           #  voters[1, group_id1 := NA]
+                           #   voters[2, proposition_vote := NA]
+                           #   object <- lm(proposition_vote ~ treatment + ideology1 + log_income + Q1_immigration , weights = NULL, data = voters)
+                           #   clustid <- c("group_id1") 
+                           #   param <- "treatment"
+                           # beta0 = 0
+                           #   alpha = 0.05
+                           #   B = 100000
+                           #   weights = NULL
+                           #   conf_int = NULL 
+                           #   debug = FALSE
+                           #   seed = 1234
               
   check_arg(clustid, "character scalar | character vector")
   check_arg(param, "scalar character")
