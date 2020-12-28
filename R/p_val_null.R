ee42004931c52dad94b64f6a6789d9f620548343
Alexander Fischer <alexander-fischer1801@t-online.de>
2020-12-28 12:42:58 +0100

    add function to map A, B, CD, DD, CC to p-value

---
+++ R/p_val_null.R
@@ -0,0 +1,25 @@
+p_val_null2 <- function(beta0, A, B, CC, CD, DD, clustid, boot_iter, small_sample_correction){
+  
+  JJ <- list()
+  for(x in names(clustid)){
+    numer <- A + B * beta0
+    JJ[[x]] <- colSums(small_sample_correction[x] * (CC[[x]] + 2* CD[[x]]*beta0+ DD[[x]]* beta0^2))
+  }
+
+  JJ_sum <- Reduce("+", JJ)
+  denom <- suppressWarnings(sqrt(JJ_sum))
+
+  t <- abs(numer) / denom
+  delete_invalid_t_total <- sum(is.na(t))
+  t <- t[!is.na(t)]
+  
+  t_boot <- t[2:(boot_iter + 1 - delete_invalid_t_total)]
+  p_val <- mean(abs(t[1] - beta0) < (t_boot))
+  
+  res <- list(p_val = p_val, 
+              t = t, 
+              t_boot = t_boot, 
+              invalid_t = delete_invalid_t_total)
+  
+}
+
