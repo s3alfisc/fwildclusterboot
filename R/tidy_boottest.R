6ecb453b45f233d0f41c547e759e4e3b509eb6e4
Alexander Fischer <alexander-fischer1801@t-online.de>
2020-12-30 11:10:31 +0100

    change summarize_boot function to summary() method, plot to plot() method

--- R/tidy_boottest.R
+++ R/tidy_boottest.R
@@ -6,14 +6,21 @@
    UseMethod("tidy", x)
  }
   
- summarize_boot <- function(x, ...){
+ summary <- function(x, ...){
    #' S3 method to summarize objects of class boottest 
    #'@export 
    #'@param x object of type boottest
    #'@param ... other arguments
-   UseMethod("summarize_boot", x)
+   UseMethod("summary", x)
  }
 
+plot <- function(x, ...){
+   #' S3 generic to plot bootstrap t statistics
+   #'@export 
+   #'@param x object of type boottest
+   #'@param ... other arguments
+   UseMethod("plot", x)
+}
 
 tidy.boottest <- function(object){
   #' S3 method to summarize objects of class boottest into tidy data.frame
@@ -45,12 +52,12 @@ tidy.boottest <- function(object){
   
 }
 
-summarize_boot.boottest <- function(object, digits = 3){
+summary.boottest <- function(object, digits = 3){
   #' S3 method to summarize objects of class boottest 
   #'@export 
   #'@param object object of type boottest
   #'@param digits rounding of output
-  #'@method summarize_boot boottest
+  #'@method summary boottest
 
   
   stopifnot(inherits(object, "boottest"))
@@ -58,9 +65,20 @@ summarize_boot.boottest <- function(object, digits = 3){
   
   N <- object$N
   B <- object$B
-  depvar <- object$depvar
+  alpha <- object$alpha
+  signif_level <- paste0((1 - alpha) * 100, "%")
+  call <- object$call
+  N_G <- object$N_G
+  B <- object$B
   #clustid <- 
   estim_function <- class(object$regression)
+  if(length(object$clustid) == 1){
+    clustering_type <- "oneway"  
+    numb_clusters <- N_G
+  } else if(length(object$clustid) == 2){
+    clustering_type <- "twoway"
+    numb_clusters <- paste(N_G[1], "x", N_G[2])
+  }
   numb_clusters <- object$N_G
   
   #if(class(object$regression) %in% c("lm", "lm_robust", "felm")){
@@ -74,35 +92,39 @@ summarize_boot.boottest <- function(object, digits = 3){
   #tidy_object <- as.data.frame(round(tidy_object, digits = 3))
   #rownames(tidy_object) <- treatment_name
   
+  print(call)
   cat("\t\n", 
-      sprintf("OLS estimation, Dep.Var: %s\n", depvar), 
-      sprintf("Estimation Function: %s\n", estim_function), 
-      sprintf("Observations:%s\n", N), 
-      sprintf("Standard-errors: Clustered  %s\n", ""), 
-      sprintf("Number of Clusters:  %s\n", numb_clusters), 
+      sprintf("Observations: %s\n", N), 
+      sprintf("Bootstr. Iter: %s\n", B), 
+      sprintf("Clustering: %s\n", clustering_type), 
+      sprintf("Confidence Sets: %s\n", signif_level),
+      sprintf("Number of Clusters: %s\n", numb_clusters), 
+      
       #sprintf("Adj. R-Squared: %s\n", round(adj_r_squared,6)), 
       sprintf("%s\n", "")) 
 
   return(tidy_object)
 }
 
-plot_boot <- function(object){
+plot.boottest <- function(object){
   
   #' Plot the bootstrap distribution of t-statistics
   #' @param object An object of type boottest
   #' @importFrom graphics abline grid lines
+  #' @method plot boottest
   #' @export 
   stopifnot(inherits(object, "boottest"))
   
   test_vals <- object$test_vals
   p_test_vals <- object$p_test_vals
   conf_int <- object$conf_int
+  alpha <- object$alpha
   
-  plot(test_vals, p_test_vals,  type = "b", pch = 20, lty = 2, xlab = "Constraint", ylab = "p-value")
+  graphics::plot(x = test_vals, y = p_test_vals,  type = "b", pch = 20, lty = 2, xlab = "Constraint", ylab = "p-value")
   lines(test_vals, p_test_vals, type = "l", lty = 1)
   abline(v=conf_int[1], col = "blue")
   abline(v=conf_int[2], col = "blue")
-  abline(h = 0.05, col = "red")
+  abline(h = alpha, col = "red")
   
   
 }
