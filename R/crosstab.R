cc26a8689e4f0806d702eea060f0d86c962663be
Alexander Fischer <alexander-fischer1801@t-online.de>
2021-01-03 16:48:31 +0100

    delete crosstab3

--- R/crosstab.R
+++ R/crosstab.R
@@ -1,34 +1,3 @@
-crosstab3 <- function(data, var1, var2){
-  
-  #' Function 3 to calculate crosstabs
-  #' @param data A matrix to collapse by two dimensions var1 var2
-  #' @param var1 a data.frame containing a single variable 
-  #' @param var2 a data.frame containing a single variable
-  #' @return A collapsed matrix of dimension length(unique(var1)) x length(unique(var2)). If...
-  
-  unique_var1 <- as.vector(unique(var1[, names(var1)]))
-  unique_var2 <- unique(var2)
-  unique_var1_len <- length(unique_var1)
-  unique_var2_len <- length(unique_var2)
-  
-  data <- as.vector(data)
-  add_zeros <- nrow(unique(expand.grid(var1 = unique_var1, var2 = unique_var2))) - length(data)
-  data <- c(data, rep(0, add_zeros))
-  res <- collapse::fsum(x = y, g = data.frame(grid))
-  dim(res) <- c(unique_var1_len, unique_var2_len)
-  res
-  
-}
-
-# set.seed(1)
-# a <- sample(1:5, 10, replace = TRUE)
-# b <- sample(1:3, 10, replace = TRUE)
-# y <- matrix(rnorm(10), 10, 1)
-# a <- as.data.frame(a)
-# b <- as.data.frame(b)
-# 
-# crosstab2(as.matrix(y), var1 = a, var2 = b)
-
 crosstab2<- function(data, var1, var2){
   
   #' Function 2 to calculate crosstabs
@@ -123,6 +92,27 @@ crosstab4 <- function(data, var1, var2){
 #  values
 #}
 
+#crosstab3 <- function(data, var1, var2){
+#  
+#  #' Function 3 to calculate crosstabs
+#  #' @param data A matrix to collapse by two dimensions var1 var2
+#  #' @param var1 a data.frame containing a single variable 
+#  #' @param var2 a data.frame containing a single variable
+#  #' @return A collapsed matrix of dimension length(unique(var1)) x length(unique(var2)). If...
+#  
+#  unique_var1 <- as.vector(unique(var1[, names(var1)]))
+#  unique_var2 <- unique(var2)
+#  unique_var1_len <- length(unique_var1)
+#  unique_var2_len <- length(unique_var2)
+#  
+#  data <- as.vector(data)
+#  add_zeros <- nrow(unique(expand.grid(var1 = unique_var1, var2 = unique_var2))) - length(data)
+#  data <- c(data, rep(0, add_zeros))
+#  res <- collapse::fsum(x = y, g = data.frame(grid))
+#  dim(res) <- c(unique_var1_len, unique_var2_len)
+#  res
+#  
+#}
 
 # a <- sample(1:40, 10000, replace = TRUE)
 # b <- sample(1:100, 10000, replace = TRUE)
