c3be5a5cd6743743d108607d76436e328f2b7fd4
Alexander Fischer <alexander-fischer1801@t-online.de>
2021-01-06 22:24:53 +0100

    add method, test

---
+++ inst/tinytest/test_15_boottest_args.R
@@ -0,0 +1,62 @@
+# test boottest args alpha, type
+
+lm_fit <- lm(proposition_vote ~ treatment + ideology1 + log_income + Q1_immigration , 
+             data = create_data_2(N = 1000, N_G1 = 20, icc1 = 0.01, N_G2 = 10, icc2 = 0.01, numb_fe1 = 10, numb_fe2 = 10, seed = 1234))
+
+# increase alphas
+alphas <- c(0.01, 0.05, 0.1, 0.2, 0.5, 0.9)
+res <- 
+lapply(alphas, function(x){
+  tmp <- boottest(object = lm_fit, clustid =  "group_id1", B = 1000, seed = 911, param = "treatment", conf_int = TRUE, alpha = x)
+  tidy(tmp)
+})
+
+lower <- rbindlist(res)$`CI Lower`
+upper <- rbindlist(res)$`CI Upper`
+
+expect_equal(sort(lower), lower)
+expect_equal(sort(upper, decreasing = TRUE), upper)
+
+
+# try out other distributions
+dists <- c("rademacher", "mammen", "norm","webb")
+res <- 
+  lapply(dists, function(x){
+    tmp <- boottest(object = lm_fit, clustid =  "group_id1", B = 10000, seed = 911, param = "treatment", conf_int = TRUE, type = x)
+    tidy(tmp)
+  })
+
+p_vals <- round(rbindlist(res)$`Pr(>|t|)`, digits = 2)
+
+expect_true(p_vals[1] == p_vals[2] &  p_vals[2] == p_vals[3] &  p_vals[3] == p_vals[4])
+
+
+# feols
+feols_fit <- fixest::feols(proposition_vote ~ treatment + ideology1 + log_income + Q1_immigration, 
+                           data = create_data_2(N = 1000, N_G1 = 20, icc1 = 0.01, N_G2 = 10, icc2 = 0.01, numb_fe1 = 10, numb_fe2 = 10, seed = 1234))
+# increase alphas
+alphas <- c(0.01, 0.05, 0.1, 0.2, 0.5, 0.9)
+res <- 
+  lapply(alphas, function(x){
+    tmp <- boottest(object = feols_fit, clustid =  "group_id1", B = 1000, seed = 911, param = "treatment", conf_int = TRUE, alpha = x)
+    tidy(tmp)
+  })
+
+lower <- rbindlist(res)$`CI Lower`
+upper <- rbindlist(res)$`CI Upper`
+
+expect_equal(sort(lower), lower)
+expect_equal(sort(upper, decreasing = TRUE), upper)
+
+
+# try out other distributions
+dists <- c("rademacher", "mammen", "norm","webb")
+res <- 
+  lapply(dists, function(x){
+    tmp <- boottest(object = feols_fit, clustid =  "group_id1", B = 10000, seed = 911, param = "treatment", conf_int = TRUE, type = x)
+    tidy(tmp)
+  })
+
+p_vals <- round(rbindlist(res)$`Pr(>|t|)`, digits = 2)
+
+expect_true(p_vals[1] == p_vals[2] &  p_vals[2] == p_vals[3] &  p_vals[3] == p_vals[4])
