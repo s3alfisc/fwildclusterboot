731eb5ce282e43b222d41943909c9dc903a848cf
Alexander Fischer <alexander-fischer1801@t-online.de>
2021-01-11 12:54:56 +0100

    colSums already in boot_algo

--- R/p_val_null.R
+++ R/p_val_null.R
@@ -17,12 +17,13 @@ p_val_null2 <- function(beta0, A, B, CC, CD, DD, clustid, boot_iter, small_sampl
   
   JJ <- list()
   for(x in names_clustid){
-    JJ[[x]] <- colSums(small_sample_correction[x] * (CC[[x]] + 2* CD[[x]]*beta0+ DD[[x]]* beta0^2))
+    #JJ[[x]] <- colSums(small_sample_correction[x] * (CC[[x]] + 2* CD[[x]]*beta0+ DD[[x]]* beta0^2))
+    JJ[[x]] <- (small_sample_correction[x] * (CC[[x]] + 2* CD[[x]]*beta0+ DD[[x]]* beta0^2))
   }
 
   len_names_clustid <- length(names_clustid)
   if(len_names_clustid == 1){
-    JJ_sum <- unlist(JJ)
+    JJ_sum <- JJ[["clustid"]]
   } else if(len_names_clustid > 1){
     JJ_sum <- Reduce("+", JJ)
   }
@@ -34,8 +35,21 @@ p_val_null2 <- function(beta0, A, B, CC, CD, DD, clustid, boot_iter, small_sampl
   t <- t[!is.na(t)]
   
   t_boot <- t[2:(boot_iter + 1 - delete_invalid_t_total)]
-  p_val <- mean(abs(t[1] - beta0) < (t_boot))
   
+  #if(p_val == "two-tailed symmetric"){
+    p_val <- mean(abs(t[1] - beta0) < (t_boot))
+  # } else if(p_val == "equal-tailed"){
+  #   p_l <- mean((t[1] - beta0) < t_boot)
+  #   p_h <- mean((t[1] - beta0) > t_boot)
+  #   p_val <- 2*min(p_l, p_h)
+  # } else if(p_val == "<"){
+  #   p_l <- mean((t[1] - beta0) < t_boot)
+  #   p_val <- p_l
+  # } else if(p_val == ">"){
+  #   p_h <- mean((t[1] - beta0) > t_boot)
+  #   p_val <- p_h
+  # }
+
   res <- list(p_val = p_val, 
               t = t, 
               t_boot = t_boot, 
