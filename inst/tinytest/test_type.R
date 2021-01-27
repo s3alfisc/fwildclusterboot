1e2ff62ddbe60595cd74f74f13e1b9679b260c1c
Alexander Fischer <alexander-fischer1801@t-online.de>
2021-01-27 18:58:44 +0100

    add tests

--- inst/tinytest/test_type.R
+++ inst/tinytest/test_type.R
@@ -1,6 +1,9 @@
 # ------------------------------------------------------------------ # 
 # test 1: bootcluster 
 # Tests A1 and A2
+# note: all tests without fixed effects should be identical, 
+# with fixed effects: because point estimate are slightly different, 
+# everything else might be slightly different (p-values, confidence intervals etc)
 # ------------------------------------------------------------------ # 
 
 library(fwildclusterboot)
@@ -26,11 +29,11 @@ feols_fit_c <- fixest::feols(proposition_vote ~ treatment + ideology1 + log_inco
 felm_fit_c <- lfe::felm(proposition_vote ~ treatment + ideology1 + log_income + Q1_immigration | 0 | 0 | group_id1,
                         data = fwildclusterboot:::create_data_2(N = 10000, N_G1 = 20, icc1 = 0.01, N_G2 = 10, icc2 = 0.01, numb_fe1 = 10, numb_fe2 = 10, seed = 1234))
 
-boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  "group_id1", B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "rademacher"))
-boot_fixest <- suppressWarnings(boottest(object = feols_fit, clustid = c("group_id1"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "rademacher"))
-boot_felm <- suppressWarnings(boottest(object = felm_fit, clustid =  "group_id1", B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "rademacher"))
-boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, clustid = c("group_id1"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "rademacher"))
-boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c, clustid =  "group_id1", B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "rademacher"))
+boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  "group_id1", B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "rademacher"))
+boot_fixest <- suppressWarnings(boottest(object = feols_fit, clustid = c("group_id1"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "rademacher"))
+boot_felm <- suppressWarnings(boottest(object = felm_fit, clustid =  "group_id1", B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "rademacher"))
+boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, clustid = c("group_id1"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "rademacher"))
+boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c, clustid =  "group_id1", B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "rademacher"))
 
 # point estimates
 expect_equivalent(boot_lm$point_estimate, boot_fixest$point_estimate)
@@ -70,11 +73,11 @@ expect_equivalent(boot_felm_c$conf_int, boot_lm$conf_int, tolerance = 1e-2)
 # Part A2: no fixed effect in model
 
 
-boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "rademacher"))
-boot_fixest <- suppressWarnings(boottest(object = feols_fit, clustid = c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "rademacher"))
-boot_felm <- suppressWarnings(boottest(object = felm_fit, clustid =  c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "rademacher"))
-boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, clustid = c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "rademacher"))
-boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c, clustid =  c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "rademacher"))
+boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "rademacher"))
+boot_fixest <- suppressWarnings(boottest(object = feols_fit, clustid = c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "rademacher"))
+boot_felm <- suppressWarnings(boottest(object = felm_fit, clustid =  c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "rademacher"))
+boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, clustid = c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "rademacher"))
+boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c, clustid =  c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "rademacher"))
 
 # point estimates
 expect_equivalent(boot_lm$point_estimate, boot_fixest$point_estimate)
@@ -110,11 +113,11 @@ expect_equivalent(boot_felm_c$conf_int, boot_lm$conf_int, tolerance = 1e-2)
 # Part A1: no fixed effect in model: webb
 
 
-boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  "group_id1", B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "webb"))
-boot_fixest <- suppressWarnings(boottest(object = feols_fit, clustid = c("group_id1"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "webb"))
-boot_felm <- suppressWarnings(boottest(object = felm_fit, clustid =  "group_id1", B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "webb"))
-boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, clustid = c("group_id1"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "webb"))
-boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c, clustid =  "group_id1", B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "webb"))
+boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  "group_id1", B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "webb"))
+boot_fixest <- suppressWarnings(boottest(object = feols_fit, clustid = c("group_id1"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "webb"))
+boot_felm <- suppressWarnings(boottest(object = felm_fit, clustid =  "group_id1", B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "webb"))
+boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, clustid = c("group_id1"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "webb"))
+boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c, clustid =  "group_id1", B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "webb"))
 
 # point estimates
 expect_equivalent(boot_lm$point_estimate, boot_fixest$point_estimate)
@@ -153,11 +156,11 @@ expect_equivalent(boot_felm_c$conf_int, boot_lm$conf_int, tolerance = 1e-2)
 # ---------------------------------------------------------------------------------------------- # 
 # Part A2: no fixed effect in model
 
-boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "webb"))
-boot_fixest <- suppressWarnings(boottest(object = feols_fit, clustid = c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "webb"))
-boot_felm <- suppressWarnings(boottest(object = felm_fit, clustid =  c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "webb"))
-boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, clustid = c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "webb"))
-boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c, clustid =  c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "webb"))
+boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "webb"))
+boot_fixest <- suppressWarnings(boottest(object = feols_fit, clustid = c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "webb"))
+boot_felm <- suppressWarnings(boottest(object = felm_fit, clustid =  c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "webb"))
+boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, clustid = c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "webb"))
+boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c, clustid =  c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "webb"))
 
 # point estimates
 expect_equivalent(boot_lm$point_estimate, boot_fixest$point_estimate)
@@ -194,11 +197,11 @@ expect_equivalent(boot_felm_c$conf_int, boot_lm$conf_int, tolerance = 1e-2)
 # ---------------------------------------------------------------------------------------------- # 
 # Part A1: no fixed effect in model: norm
 
-boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  "group_id1", B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "norm"))
-boot_fixest <- suppressWarnings(boottest(object = feols_fit, clustid = c("group_id1"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "norm"))
-boot_felm <- suppressWarnings(boottest(object = felm_fit, clustid =  "group_id1", B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "norm"))
-boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, clustid = c("group_id1"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "norm"))
-boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c, clustid =  "group_id1", B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "norm"))
+boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  "group_id1", B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "norm"))
+boot_fixest <- suppressWarnings(boottest(object = feols_fit, clustid = c("group_id1"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "norm"))
+boot_felm <- suppressWarnings(boottest(object = felm_fit, clustid =  "group_id1", B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "norm"))
+boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, clustid = c("group_id1"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "norm"))
+boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c, clustid =  "group_id1", B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "norm"))
 
 # point estimates
 expect_equivalent(boot_lm$point_estimate, boot_fixest$point_estimate)
@@ -237,11 +240,11 @@ expect_equivalent(boot_felm_c$conf_int, boot_lm$conf_int, tolerance = 1e-2)
 # ---------------------------------------------------------------------------------------------- # 
 # Part A2: no fixed effect in model
 
-boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "norm"))
-boot_fixest <- suppressWarnings(boottest(object = feols_fit, clustid = c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "norm"))
-boot_felm <- suppressWarnings(boottest(object = felm_fit, clustid =  c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "norm"))
-boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, clustid = c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "norm"))
-boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c, clustid =  c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "norm"))
+boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "norm"))
+boot_fixest <- suppressWarnings(boottest(object = feols_fit, clustid = c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "norm"))
+boot_felm <- suppressWarnings(boottest(object = felm_fit, clustid =  c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "norm"))
+boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, clustid = c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "norm"))
+boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c, clustid =  c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "norm"))
 
 # point estimates
 expect_equivalent(boot_lm$point_estimate, boot_fixest$point_estimate)
@@ -276,38 +279,40 @@ expect_equivalent(boot_felm_c$conf_int, boot_lm$conf_int, tolerance = 1e-2)
 
 # ---------------------------------------------------------------------------------------------- # 
 # Part A1: no fixed effect in model: mammen
+# 
 
 boot_lm <-  suppressWarnings(boottest(object = lm_fit, 
                                       clustid =  "group_id1", 
-                                      B = 999,
-                                      seed = 911, 
+                                      B = 9999,
+                                      seed = 91, 
                                       param = "treatment", 
                                       conf_int = TRUE, 
                                       type = "mammen"))
 boot_fixest <- suppressWarnings(boottest(object = feols_fit,
                                          clustid = c("group_id1"), 
-                                         B = 999,
-                                         seed = 911, 
+                                         B = 9999,
+                                         seed = 91, 
                                          param = "treatment",
                                          conf_int = TRUE,
                                          type = "mammen"))
 boot_felm <- suppressWarnings(boottest(object = felm_fit, 
                                        clustid =  "group_id1",
-                                       B = 999, 
-                                       seed = 911,
+                                       B = 9999, 
+                                       seed = 91,
                                        param = "treatment",
                                        conf_int = TRUE,
                                        type = "mammen"))
 boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, 
-                                           clustid = c("group_id1"),
-                                           B = 999, seed = 911, 
+                                           clustid = "group_id1",
+                                           B = 9999, 
+                                           seed = 91, 
                                            param = "treatment",
                                            conf_int = TRUE, 
                                            type = "mammen"))
 boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c,
                                          clustid =  "group_id1", 
-                                         B = 999, 
-                                         seed = 911,
+                                         B = 9999, 
+                                         seed = 91,
                                          param = "treatment", 
                                          conf_int = TRUE,
                                          type = "mammen"))
@@ -339,6 +344,9 @@ expect_equivalent(boot_felm$conf_int, boot_fixest_c$conf_int, tolerance = 1e-2)
 expect_equivalent(boot_fixest_c$conf_int, boot_felm_c$conf_int, tolerance = 1e-2)
 expect_equivalent(boot_felm_c$conf_int, boot_lm$conf_int, tolerance = 1e-2)
 
+# all outputs need to be equal 
+#lapply(1:length(boot_felm_c),function(i) expect_equivalent(boot_felm_c[[i]], boot_fixest_c[[i]]))
+
 
 
 # ---------------------------------------------------------------------------------------------- # 
@@ -348,11 +356,11 @@ expect_equivalent(boot_felm_c$conf_int, boot_lm$conf_int, tolerance = 1e-2)
 # ---------------------------------------------------------------------------------------------- # 
 # Part A2: no fixed effect in model
 
-boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "mammen"))
-boot_fixest <- suppressWarnings(boottest(object = feols_fit, clustid = c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "mammen"))
-boot_felm <- suppressWarnings(boottest(object = felm_fit, clustid =  c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "mammen"))
-boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, clustid = c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "mammen"))
-boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c, clustid =  c("group_id1", "group_id2"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, type = "mammen"))
+boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "mammen"))
+boot_fixest <- suppressWarnings(boottest(object = feols_fit, clustid = c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "mammen"))
+boot_felm <- suppressWarnings(boottest(object = felm_fit, clustid =  c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "mammen"))
+boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, clustid = c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "mammen"))
+boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c, clustid =  c("group_id1", "group_id2"), B = 999, seed = 91, param = "treatment", conf_int = TRUE, type = "mammen"))
 
 
 # point estimates
