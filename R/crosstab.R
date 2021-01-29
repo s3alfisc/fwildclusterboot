7ca56b6e18897f31f506acd4f57b183e6d037932
Alexander Fischer <alexander-fischer1801@t-online.de>
2021-01-29 18:31:45 +0100

    add crosstab() based on collapse

--- R/crosstab.R
+++ R/crosstab.R
@@ -55,68 +55,27 @@ crosstab4 <- function(data, var1, var2) {
   res
 }
 
-# crosstab<- function(data, var1, var2){
-#
-#  #' Function 1 to calculate crosstabs
-#  #' @param data A matrix to collapse by two dimensions var1 var2
-#  #' @param var1 a data.frame containing a single variable
-#  #' @param var2 a data.frame containing a single variable
-#  #' @return A collapsed matrix of dimension length(unique(var1)) x length(unique(var2)). If...
-#
-#  dreamerr::check_arg(var1, "data.frame")
-#  dreamerr::check_arg(var2, "data.frame")
-#
-#  # data <- dt
-#  # var1 <- "a"
-#  # var2 <- "b"
-#  # data <- XinvXXr
-#  # var1 = clustid
-#  # var2 = fixed_effect
-#
-#  unique_var1 <- as.vector(unique(var1[, names(var1)]))
-#  unique_var2 <- unique(var2)
-#
-#  unique_var1_len <- length(unique_var1)
-#  unique_var2_len <- length(unique_var2)
-#  df <- expand.grid(unique_var1, unique_var2)
-#  # create rownames as collapse::fsum
-#  df <- data.frame(names = paste0(df[, 1], ".", df[, 2]))
-#  setDT(df)
-#  res <- collapse::fsum(data, cbind(var1,var2))
-#  res_dt <- data.table(names = rownames(res), values = res)
-#  final <- merge(df, res_dt, by = "names", all.x = TRUE)
-#  values <- final[, names:=NULL]
-#  values <- matrix(as.matrix(values), unique_var1_len, unique_var2_len)
-#  values[is.na(values)] <- 0
-#  values
-# }
 
-# crosstab3 <- function(data, var1, var2){
-#
-#  #' Function 3 to calculate crosstabs
-#  #' @param data A matrix to collapse by two dimensions var1 var2
-#  #' @param var1 a data.frame containing a single variable
-#  #' @param var2 a data.frame containing a single variable
-#  #' @return A collapsed matrix of dimension length(unique(var1)) x length(unique(var2)). If...
-#
-#  unique_var1 <- as.vector(unique(var1[, names(var1)]))
-#  unique_var2 <- unique(var2)
-#  unique_var1_len <- length(unique_var1)
-#  unique_var2_len <- length(unique_var2)
-#
-#  data <- as.vector(data)
-#  add_zeros <- nrow(unique(expand.grid(var1 = unique_var1, var2 = unique_var2))) - length(data)
-#  data <- c(data, rep(0, add_zeros))
-#  res <- collapse::fsum(x = y, g = data.frame(grid))
-#  dim(res) <- c(unique_var1_len, unique_var2_len)
-#  res
-#
-# }
+crosstab <- function(data, var1, var2){
+  
+  #' collapse way to calculate crosstabs
+  #' @param data A matrix to collapse by two dimensions var1 var2
+  #' @param var1 a data.frame containing a single variable
+  #' @param var2 a data.frame containing a single variable
+  #' @return A collapsed matrix of dimension length(unique(var1)) x length(unique(var2)). If...
+  
+  
+  dreamerr::check_arg(var1, "data.frame")
+  dreamerr::check_arg(var2, "data.frame")
+  
+  length_var1 <- nrow(unique(var1))
+  length_var2 <- nrow(unique(var2))
+  
+  res <- 
+    collapse::fsum(x = data, g = collapse::qF(var1[, 1]):collapse::qF(var2[, 1]))
+  
+  res <- matrix(res, length_var1, length_var2, byrow = TRUE)
+  res[is.na(res)] <- 0
+  res
+}
 
-# a <- sample(1:40, 10000, replace = TRUE)
-# b <- sample(1:100, 10000, replace = TRUE)
-# y <- matrix(rnorm(10000), 10000, 1)
-# data <- y
-# var1 <- data.frame(a = a)
-# var2 <- data.frame(b = b)
-# microbenchmark(crosstab2(data = data, var1 = var1, var2 = var2), crosstab4(data = data, var1 = var1, var2 = var2), times = 20)
