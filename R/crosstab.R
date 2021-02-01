cea78e0d5060a5b3889d9811ae9394d9fc084954
Alexander Fischer <alexander-fischer1801@t-online.de>
2021-02-01 21:21:24 +0100

    cmd check

--- R/crosstab.R
+++ R/crosstab.R
@@ -1,35 +1,35 @@
-#' crosstab2 <- function(data, var1, var2) {
-#' 
-#'   #' Function 2 to calculate crosstabs
-#'   #' @param data A matrix to collapse by two dimensions var1 var2
-#'   #' @param var1 a data.frame containing a single variable
-#'   #' @param var2 a data.frame containing a single variable
-#'   #' @return A collapsed matrix of dimension length(unique(var1)) x length(unique(var2)). If...
-#' 
-#'   # data = XinvXXr
-#'   # var1 = clustid
-#'   # var2 = fixed_effect
-#'   dreamerr::check_arg(var1, "data.frame")
-#'   dreamerr::check_arg(var2, "data.frame")
-#' 
-#'   dt <- data.table(var1 = var1, var2 = var2, y = data)
-#'   setnames(dt, names(dt), c("var1", "var2", "y"))
-#'   setkeyv(dt, c("var1", "var2"))
-#' 
-#'   dt <- dt[CJ(var1, var2, unique = TRUE), lapply(.SD, sum), by = .EACHI]
-#'   dt[is.na(y), y := 0]
-#' 
-#'   unique_var1 <- as.vector(unique(var1[, names(var1)]))
-#'   # unique_var2 <- unique(var2)
-#'   unique_var2 <- as.vector(unique(var2[, names(var2)]))
-#' 
-#' 
-#'   unique_var1_len <- length(unique_var1)
-#'   unique_var2_len <- length(unique_var2)
-#' 
-#'   # dt$y
-#'   matrix(dt$y, unique_var1_len, unique_var2_len, byrow = TRUE)
-#' }
+# crosstab2 <- function(data, var1, var2) {
+# 
+#   #' Function 2 to calculate crosstabs
+#   #' @param data A matrix to collapse by two dimensions var1 var2
+#   #' @param var1 a data.frame containing a single variable
+#   #' @param var2 a data.frame containing a single variable
+#   #' @return A collapsed matrix of dimension length(unique(var1)) x length(unique(var2)). If...
+# 
+#   # data = XinvXXr
+#   # var1 = clustid
+#   # var2 = fixed_effect
+#   dreamerr::check_arg(var1, "data.frame")
+#   dreamerr::check_arg(var2, "data.frame")
+# 
+#   dt <- data.table(var1 = var1, var2 = var2, y = data)
+#   setnames(dt, names(dt), c("var1", "var2", "y"))
+#   setkeyv(dt, c("var1", "var2"))
+# 
+#   dt <- dt[CJ(var1, var2, unique = TRUE), lapply(.SD, sum), by = .EACHI]
+#   dt[is.na(y), y := 0]
+# 
+#   unique_var1 <- as.vector(unique(var1[, names(var1)]))
+#   # unique_var2 <- unique(var2)
+#   unique_var2 <- as.vector(unique(var2[, names(var2)]))
+# 
+# 
+#   unique_var1_len <- length(unique_var1)
+#   unique_var2_len <- length(unique_var2)
+# 
+#   # dt$y
+#   matrix(dt$y, unique_var1_len, unique_var2_len, byrow = TRUE)
+# }
 
 
 crosstab4 <- function(data, var1, var2) {
