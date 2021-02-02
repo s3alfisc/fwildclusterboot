1be9ac0de5bc683d16463b3e658e27bfcf7c8258
Alexander Fischer <alexander-fischer1801@t-online.de>
2021-02-02 12:21:24 +0100

    update tests / tests for new function arguments

--- inst/tinytest/test_error_warning.R
+++ inst/tinytest/test_error_warning.R
@@ -1,6 +1,7 @@
 # ------------------------------------------------------------------ # 
 # test for warnings and errors
 # ------------------------------------------------------------------ # 
+options(warn=-1)
 
 # test boottest function arguments for errors 
 lm_fit <- lm(proposition_vote ~ treatment + ideology1 + log_income + Q1_immigration , 
@@ -54,4 +55,189 @@ felm_fit <- lfe::felm(proposition_vote ~ treatment + ideology1 + log_income + Q1
                       data = fwildclusterboot:::create_data_2(N = 10000, N_G1 = 20, icc1 = 0.01, N_G2 = 10, icc2 = 0.01, numb_fe1 = 10, numb_fe2 = 10, seed = 1234))
 expect_error(boottest(object = felm_fit, clustid =  "group_id1", B = 999, seed = 911, param = "treatment", conf_int = TRUE, fe = "Q1_immigration"))
 expect_error(boottest(object = feols_fit, clustid = c("group_id1"), B = 999, seed = 911, param = "treatment", conf_int = TRUE, fe = "Q1_immigration"))
+
+# nthreads < 1
+
+expect_error(boottest(object = lm_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      nthreads = -1))
+expect_warning(boottest(object = lm_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      nthreads = 20))
+
+expect_error(boottest(object = feols_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      nthreads = -1))
+expect_warning(boottest(object = feols_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      nthreads = 20))
+
+expect_error(boottest(object = felm_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      nthreads = -1))
+expect_warning(boottest(object = felm_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      nthreads = 20))
+
+# maxiter 
+expect_error(boottest(object = lm_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      maxiter = -1))
+expect_error(boottest(object = lm_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      maxiter = 0.1))
+
+expect_error(boottest(object = feols_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      maxiter = -1))
+expect_error(boottest(object = feols_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      maxiter = 0.1))
+
+expect_error(boottest(object = felm_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      maxiter = -1))
+expect_error(boottest(object = felm_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      maxiter = 0.1))
+
+
+# tol
+expect_error(boottest(object = lm_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      tol = -1))
+
+expect_error(boottest(object = feols_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      tol = -1))
+
+expect_error(boottest(object = felm_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      tol = -1))
+
+# p-val type
+expect_error(boottest(object = lm_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      p_val_type = ">"))
+
+expect_error(boottest(object = feols_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      p_val_type = ">"))
+
+expect_error(boottest(object = felm_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE, 
+                      p_val_type = ">"))
+
+# B = 1000
+expect_message(boottest(object = lm_fit,
+                      clustid =  "group_id1",
+                      B = 1000, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE))
+
+expect_message(boottest(object = feols_fit,
+                      clustid =  "group_id1",
+                      B = 1000, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE))
+
+expect_message(boottest(object = felm_fit,
+                      clustid =  "group_id1",
+                      B = 1000, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE))
+
+# banned function arguments 
+
+
+# 1) felm 
+felm_fit <- lfe::felm(proposition_vote ~ treatment + ideology1 + log_income + Q1_immigration, 
+                      data = fwildclusterboot:::create_data_2(N = 1000, N_G1 = 10, icc1 = 0.01, N_G2 = 10, icc2 = 0.01, numb_fe1 = 10, numb_fe2 = 10, seed = 1234), 
+                      subset = sample(c(TRUE, FALSE), 1000, TRUE))
+expect_error(boottest(object = felm_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE))
+
+# 2) lm 
+lm_fit <- lm(proposition_vote ~ treatment + ideology1 + log_income + Q1_immigration , 
+             data = fwildclusterboot:::create_data_2(N = 1000, N_G1 = 10, icc1 = 0.01, N_G2 = 10, icc2 = 0.01, numb_fe1 = 10, numb_fe2 = 10, seed = 1234), 
+             subset = sample(c(TRUE, FALSE), 1000, TRUE))
+expect_error(boottest(object = lm_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE))
+
+# fixest
+feols_fit <- fixest::feols(proposition_vote ~ treatment + ideology1 + i(log_income,Q1_immigration), 
+                           data = fwildclusterboot:::create_data_2(N = 1000, N_G1 = 10, icc1 = 0.01, N_G2 = 10, icc2 = 0.01, numb_fe1 = 10, numb_fe2 = 10, seed = 1234))
+expect_error(boottest(object = feols_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE))
+feols_fit <- fixest::feols(proposition_vote ~ treatment + ideology1, 
+                           data = fwildclusterboot:::create_data_2(N = 1000, N_G1 = 10, icc1 = 0.01, N_G2 = 10, icc2 = 0.01, numb_fe1 = 10, numb_fe2 = 10, seed = 1234), 
+                           subset = sample(c(TRUE, FALSE), 1000, TRUE))
+expect_error(boottest(object = feols_fit,
+                      clustid =  "group_id1",
+                      B = 999, seed = 911, 
+                      param = "treatment",
+                      conf_int = TRUE))
 
