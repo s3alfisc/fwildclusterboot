d4bfa8955ac8484af02df57c58ee0e41ff168182
Alexander Fischer <alexander-fischer1801@t-online.de>
2021-02-04 12:40:03 +0100

    update voters data so that it is compatible with R < 3.5.0

--- R/voters-data.R
+++ R/voters-data.R
@@ -2,7 +2,7 @@
 # 
 # # create a data set with fwildclusterboot's create_data_2 function
 # voters <- create_data_2(N = 300, N_G1 = 40, icc1 = 0.01, N_G2 = 20, icc2 = 0.01, numb_fe1 = 10, numb_fe2 = 10, seed = 5)
-# save(voters, file = "C:/Users/alexa/Dropbox/fwildclusterboot/data/voters.RData", voters)
+# save(voters, file = "C:/Users/alexa/Dropbox/fwildclusterboot/data/voters.RData", voters, version = 2)
 
 #' Random example data set created via create_data_2 function
 #'
