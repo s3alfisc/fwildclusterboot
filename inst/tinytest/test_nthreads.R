dbf7a68c45ba195462e73959828de4ba2ffb9989
Alexander Fischer <alexander-fischer1801@t-online.de>
2021-02-26 11:25:25 +0100

    run nthreads tests only on dev versions - somehow one test fails on oracle fedora on cran

--- inst/tinytest/test_nthreads.R
+++ inst/tinytest/test_nthreads.R
@@ -2,6 +2,23 @@
 
 # test tol & maxiter arguments
 
+
+## force tests to be executed if in dev release which we define as
+## having a sub-release, eg 0.9.15.5 is one whereas 0.9.16 is not
+## code taken from https://stackoverflow.com/questions/36166288/skip-tests-on-cran-but-run-locally
+## answer provided by user Dirk Eddelbuettel & edited by Anirban166
+
+if (length(strsplit(packageDescription("fwildclusterboot")$Version, "\\.")[[1]]) > 3) { 
+  #Sys.setenv("RunAllfwildclusterbootTests"="yes")
+  runThisTest <- TRUE
+} else {
+  runThisTest <- FALSE
+}
+
+#.runThisTest <- Sys.getenv("RunAllfwildclusterbootTests") == "yes"
+
+if (runThisTest) {
+  # skip data tests if not development version x.x.x.x
 library(fwildclusterboot)
 library(lfe)
 library(fixest)
@@ -20,11 +37,15 @@ feols_fit_c <- fixest::feols(proposition_vote ~ treatment + ideology1 + log_inco
 felm_fit_c <- lfe::felm(proposition_vote ~ treatment + ideology1 + log_income + Q1_immigration | 0 | 0 | group_id1,
                         data = fwildclusterboot:::create_data(N = 10000, N_G1 = 20, icc1 = 0.01, N_G2 = 10, icc2 = 0.01, numb_fe1 = 10, numb_fe2 = 10, seed = 1234))
 
-boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  "group_id1", B = 2999, seed = 911, param = "treatment", conf_int = TRUE, bootcluster = "min", tol = 1e-10, maxiter = 30, nthreads = 2))
+boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  "group_id1", B = 2999, seed = 911, param = "treatment", conf_int = TRUE, bootcluster = "min", tol = 1e-10, maxiter = 30, 
+                                      nthreads = 2))
 #boot_fixest <- suppressWarnings(boottest(object = feols_fit, clustid = c("group_id1"), B = 2999, seed = 911, param = "treatment", conf_int = TRUE, bootcluster = "min", tol = 1e-10, maxiter = 30, nthreads = 0))
-boot_felm <- suppressWarnings(boottest(object = felm_fit, clustid =  "group_id1", B = 2999, seed = 911, param = "treatment", conf_int = TRUE, bootcluster = "min", tol = 1e-10, maxiter = 30, nthreads = 1))
-boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, clustid = c("group_id1"), B = 2999, seed = 911, param = "treatment", conf_int = TRUE, bootcluster = "min", tol = 1e-10, maxiter = 30, nthreads = 0.5))
-boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c, clustid =  "group_id1", B = 2999, seed = 911, param = "treatment", conf_int = TRUE, bootcluster = "min", tol = 1e-10, maxiter = 30, nthreads = 2))
+boot_felm <- suppressWarnings(boottest(object = felm_fit, clustid =  "group_id1", B = 2999, seed = 911, param = "treatment", conf_int = TRUE, bootcluster = "min", tol = 1e-10, maxiter = 30,
+                                       nthreads = 1))
+boot_fixest_c <- suppressWarnings(boottest(object = feols_fit_c, clustid = c("group_id1"), B = 2999, seed = 911, param = "treatment", conf_int = TRUE, bootcluster = "min", tol = 1e-10, maxiter = 30, 
+                                           nthreads = 0.5))
+boot_felm_c <- suppressWarnings(boottest(object = felm_fit_c, clustid =  "group_id1", B = 2999, seed = 911, param = "treatment", conf_int = TRUE, bootcluster = "min", tol = 1e-10, maxiter = 30, 
+                                         nthreads = 2))
 
 # point estimates
 #expect_equivalent(boot_lm$point_estimate, boot_fixest$point_estimate, tolerance = 0.1)
@@ -53,3 +74,5 @@ expect_equivalent(boot_felm_c$t_stat, boot_lm$t_stat, tolerance = 0.1)
 expect_equivalent(boot_felm$conf_int, boot_fixest_c$conf_int)
 expect_equivalent(boot_fixest_c$conf_int, boot_felm_c$conf_int)
 expect_equivalent(boot_felm_c$conf_int, boot_lm$conf_int)
+
+}
