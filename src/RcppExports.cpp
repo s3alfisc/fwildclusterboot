27acf11a177187a09fc779934e2982e5417643d9
Alexander Fischer <alexander-fischer1801@t-online.de>
2021-07-29 16:44:17 +0200

    version 0.3.6

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // eigenMatMult
 SEXP eigenMatMult(Eigen::MatrixXd A, Eigen::MatrixXd B, int nthreads);
 RcppExport SEXP _fwildclusterboot_eigenMatMult(SEXP ASEXP, SEXP BSEXP, SEXP nthreadsSEXP) {
