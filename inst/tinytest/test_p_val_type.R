3d5280cb8b3721de40d376b14f4b386c0168fe23
Alexander Fischer <alexander-fischer1801@t-online.de>
2021-09-30 17:59:13 +0200

    #17 update consistency tests for p_val_type argument in boottest()

--- inst/tinytest/test_p_val_type.R
+++ inst/tinytest/test_p_val_type.R
@@ -15,39 +15,6 @@ felm_fit_c <- felm(proposition_vote ~ treatment + ideology1 + log_income + Q1_im
                    data = fwildclusterboot:::create_data(N = 1000, N_G1 = 20, icc1 = 0.1, N_G2 = 10, icc2 = 0.1, numb_fe1 = 10, numb_fe2 = 10, seed = 1234))
 
 
-
-# two-tailed
-
-# point estimates
-expect_equivalent(boot_lm$point_estimate, boot_fixest$point_estimate, tolerance = 0.1)
-expect_equivalent(boot_fixest$point_estimate, boot_felm$point_estimate, tolerance = 0.1)
-expect_equivalent(boot_felm$point_estimate, boot_fixest_c$point_estimate, tolerance = 0.1)
-expect_equivalent(boot_fixest_c$point_estimate, boot_felm_c$point_estimate, tolerance = 0.1)
-expect_equivalent(boot_felm_c$point_estimate, boot_lm$point_estimate, tolerance = 0.1)
-
-# p-vals
-expect_equivalent(boot_lm$p_val, boot_fixest$p_val, tolerance = 0.1)
-expect_equivalent(boot_fixest$p_val, boot_felm$p_val, tolerance = 0.1)
-expect_equivalent(boot_felm$p_val, boot_fixest_c$p_val, tolerance = 0.1)
-expect_equivalent(boot_fixest_c$p_val, boot_felm_c$p_val, tolerance = 0.1)
-expect_equivalent(boot_felm_c$p_val, boot_lm$p_val, tolerance = 0.1)
-
-# t_stats
-expect_equivalent(boot_lm$t_stat, boot_fixest$t_stat, tolerance = 0.1)
-expect_equivalent(boot_fixest$t_stat, boot_felm$t_stat, tolerance = 0.1)
-expect_equivalent(boot_felm$t_stat, boot_fixest_c$t_stat, tolerance = 0.1)
-expect_equivalent(boot_fixest_c$t_stat, boot_felm_c$t_stat, tolerance = 0.1)
-expect_equivalent(boot_felm_c$t_stat, boot_lm$t_stat, tolerance = 0.1)
-
-# confidence intervals
-expect_equal(boot_lm$conf_int, boot_fixest$conf_int, tolerance = 0.1)
-expect_equal(boot_fixest$conf_int, boot_felm$conf_int, tolerance = 0.1)
-expect_equal(boot_felm$conf_int, boot_fixest_c$conf_int, tolerance = 0.1)
-expect_equal(boot_fixest_c$conf_int, boot_felm_c$conf_int, tolerance = 0.1)
-expect_equal(boot_felm_c$conf_int, boot_lm$conf_int, tolerance = 0.1)
-
-
-
 # equal-tailed
 
 boot_lm <-  suppressWarnings(boottest(object = lm_fit, clustid =  "group_id1", B = 999, seed = 91, param = "treatment", conf_int = TRUE, p_val_type = "equal-tailed"))
