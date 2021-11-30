24f2d82f9fd487356c57dc03686a215274816ebd
Alexander Fischer <alexander-fischer1801@t-online.de>
2021-11-30 20:24:09 +0100

    update tests: ssc() instead of dof(); add tests for two-way clustering

--- inst/tinytest/test_small_sample_correction_tstat.R
+++ inst/tinytest/test_small_sample_correction_tstat.R
@@ -13,7 +13,9 @@ library(fixest)
 data(EmplUK)
 EmplUK$firm <- as.factor(EmplUK$firm)
 
-feols_fit <- fixest::feols(emp ~ wage + capital + output + as.factor(firm) + as.factor(year), data = EmplUK, cluster = ~firm, dof = dof(adj = FALSE,cluster.adj = TRUE))
+feols_fit <- fixest::feols(emp ~ wage + capital + output + as.factor(firm) + as.factor(year), data = EmplUK, 
+                           cluster = ~firm, 
+                           ssc = ssc(adj = FALSE,cluster.adj = TRUE))
 lm_fit <- lm(emp ~ wage + capital + output + as.factor(firm) + as.factor(year), data = EmplUK)
 
 dof_tstat <- fixest::coeftable(feols_fit)[c("wage", "capital", "output"), 3]
@@ -26,9 +28,9 @@ boot3 <- boottest(lm_fit, clustid = c("firm"), B = B, param = "output", nthreads
 
 # boottest returns absolute values of t-stats
 
-expect_equal(abs(boot1$t_stat), abs(dof_tstat[1]))
-expect_equal(abs(boot2$t_stat), abs(dof_tstat[2]))
-expect_equal(abs(boot3$t_stat), abs(dof_tstat[3]))
+expect_equivalent(abs(boot1$t_stat), abs(dof_tstat[1]))
+expect_equivalent(abs(boot2$t_stat), abs(dof_tstat[2]))
+expect_equivalent(abs(boot3$t_stat), abs(dof_tstat[3]))
 
 # note that these results should be independent of the number of 
 # bootstrap draws - in the "first" ob B + 1 bootstrap draws, all weights are set to 1
@@ -39,8 +41,53 @@ boot1 <- boottest(lm_fit, clustid = c("firm"), B = B, param = "wage", nthreads =
 boot2 <- boottest(lm_fit, clustid = c("firm"), B = B, param = "capital", nthreads = 1, seed = y, impose_null = FALSE)
 boot3 <- boottest(lm_fit, clustid = c("firm"), B = B, param = "output", nthreads = 1, seed = y, impose_null = FALSE)
 
-expect_equal(abs(boot1$t_stat), abs(dof_tstat[1]))
-expect_equal(abs(boot2$t_stat), abs(dof_tstat[2]))
-expect_equal(abs(boot3$t_stat), abs(dof_tstat[3]))
+expect_equivalent(abs(boot1$t_stat), abs(dof_tstat[1]))
+expect_equivalent(abs(boot2$t_stat), abs(dof_tstat[2]))
+expect_equivalent(abs(boot3$t_stat), abs(dof_tstat[3]))
+
+# twoway clustering 
+
+feols_fit <- fixest::feols(emp ~ wage + capital + output, data = EmplUK, 
+                           cluster = ~firm + year, 
+                           ssc = ssc(adj = FALSE,
+                                     cluster.adj = TRUE, 
+                                     cluster.df = "conventional"))
+
+lm_fit <- lm(emp ~ wage + capital + output, data = EmplUK)
+dof_tstat <- fixest::coeftable(feols_fit)[c("wage", "capital", "output"), 3]
+
+
+B <- 123
+y <- 2
+boot1 <- boottest(lm_fit, clustid = c("firm", "year"), B = B, param = "wage", nthreads = 1, seed = y, impose_null = FALSE)
+boot2 <- boottest(lm_fit, clustid = c("firm", "year"), B = B, param = "capital", nthreads = 1, seed = y, impose_null = FALSE)
+boot3 <- boottest(lm_fit, clustid = c("firm", "year"), B = B, param = "output", nthreads = 1, seed = y, impose_null = FALSE)
+
+expect_equivalent(abs(boot1$t_stat), abs(dof_tstat[1]))
+expect_equivalent(abs(boot2$t_stat), abs(dof_tstat[2]))
+expect_equivalent(abs(boot3$t_stat), abs(dof_tstat[3]))
+
+# twoway and 'min' 
+
+# feols_fit <- fixest::feols(emp ~ wage + capital + output, data = EmplUK, 
+#                            cluster = ~firm + year, 
+#                            ssc = ssc(adj = FALSE,
+#                                      cluster.adj = TRUE, 
+#                                      cluster.df = "min"))
+# 
+# lm_fit <- lm(emp ~ wage + capital + output, data = EmplUK)
+# dof_tstat <- fixest::coeftable(feols_fit)[c("wage", "capital", "output"), 3]
+# 
+# 
+# B <- 123
+# y <- 2
+# boot1 <- boottest(lm_fit, clustid = c("firm", "year"), B = B, param = "wage", nthreads = 1, seed = y, impose_null = FALSE, bootcluster = "min")
+# boot2 <- boottest(lm_fit, clustid = c("firm", "year"), B = B, param = "capital", nthreads = 1, seed = y, impose_null = FALSE, bootcluster = "min")
+# boot3 <- boottest(lm_fit, clustid = c("firm", "year"), B = B, param = "output", nthreads = 1, seed = y, impose_null = FALSE, bootcluster = "min")
+# 
+# expect_equivalent(abs(boot1$t_stat), abs(dof_tstat[1]))
+# expect_equivalent(abs(boot2$t_stat), abs(dof_tstat[2]))
+# expect_equivalent(abs(boot3$t_stat), abs(dof_tstat[3]))
+
 
 }
