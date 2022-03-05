374ab4ef1e3be8d212fc10e8200ff5dcc42acbf3
Alexander Fischer <alexander-fischer1801@t-online.de>
2022-03-05 20:50:40 +0100

    add tests

---
+++ tests/testthat/test-to_integer.R
@@ -0,0 +1,6 @@
+test_that("to_integer works", {
+  expect_identical(fwildclusterboot:::to_integer(c("a", "b", "c")), 1:3)
+  expect_identical(fwildclusterboot:::to_integer(1:3), 1:3)
+  expect_identical(fwildclusterboot:::to_integer(2:4), 1:3)
+  expect_identical(fwildclusterboot:::to_integer(factor(c(1, 2, 3))), 1:3)
+})
