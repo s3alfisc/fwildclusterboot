c19fdd428f796dcd7a76018716e65e3fc3124957
Alexander Fischer <alexander-fischer1801@t-online.de>
2022-03-05 12:25:27 +0100

    delete file

--- tests/tinytest.R
+++
@@ -1,9 +0,0 @@
-if (requireNamespace("tinytest", quietly = TRUE)) {
-  # run tests only if both lfe and fixest are installed
-  if(requireNamespace("lfe") && requireNamespace("fixest")){
-    library(lfe)
-    library(fixest)
-    tinytest::test_package("fwildclusterboot")
-  }
-}
-
