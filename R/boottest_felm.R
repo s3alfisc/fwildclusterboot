1362eb46346c0b774d1662f36205d435026ad74a
Alexander Fischer <alexander-fischer1801@t-online.de>
2022-03-19 18:24:24 +0100

    #33 fix all rmd check errors

--- R/boottest_felm.R
+++ R/boottest_felm.R
@@ -370,7 +370,8 @@ boottest.felm <- function(object,
     
     full_enumeration <- FALSE
     if(heteroskedastic == FALSE){
-      N_G_2 <- 2^preprocess$N_G_bootcluster
+      N_G_bootcluster <- preprocess$N_G_bootcluster
+      N_G_2 <- 2^N_G_bootcluster      
       if (type == "rademacher") {
         if(N_G_2 <= B){
           warning(paste("There are only", N_G_2, "unique draws from the rademacher distribution for", N_G_bootcluster, "bootstrap clusters. Therefore, B = ", N_G_2, " with full enumeration. Consider using webb weights instead. Further, note that under full enumeration and with B =", N_G_2, "bootstrap draws, only 2^(#clusters - 1) = ", 2^(N_G_bootcluster - 1), " distinct t-statistics and p-values can be computed. For a more thorough discussion, see Webb `Reworking wild bootstrap based inference for clustered errors` (2013)."),
--- R/boottest_fixest.R
+++ R/boottest_fixest.R
@@ -394,7 +394,8 @@ boottest.fixest <- function(object,
     
     full_enumeration <- FALSE
     if(heteroskedastic == FALSE){
-      N_G_2 <- 2^preprocess$N_G_bootcluster
+      N_G_bootcluster <- preprocess$N_G_bootcluster
+      N_G_2 <- 2^N_G_bootcluster
       if (type == "rademacher") {
         if(N_G_2 <= B){
           warning(paste("There are only", N_G_2, "unique draws from the rademacher distribution for", N_G_bootcluster, "bootstrap clusters. Therefore, B = ", N_G_2, " with full enumeration. Consider using webb weights instead. Further, note that under full enumeration and with B =", N_G_2, "bootstrap draws, only 2^(#clusters - 1) = ", 2^(N_G_bootcluster - 1), " distinct t-statistics and p-values can be computed. For a more thorough discussion, see Webb `Reworking wild bootstrap based inference for clustered errors` (2013)."),
@@ -522,6 +523,7 @@ boottest.fixest <- function(object,
     point_estimate <- as.vector(object$coefficients[param] %*% preprocess$R0[param])
     
     # number of clusters used in bootstrap - always derived from bootcluster
+    
     N_G_bootcluster <- preprocess$N_G_bootcluster
     N_G_2 <- 2^N_G_bootcluster
     if (type == "rademacher") {
@@ -963,7 +965,7 @@ waldboottest.fixest <- function(object,
   clustid_dims <- preprocess$clustid_dims
 
   # number of clusters used in bootstrap - always derived from bootcluster
-  N_G_bootcluster <- length(unique(preprocess$bootcluster[, 1]))
+  N_G_bootcluster <- preprocess$N_G_bootcluster
   N_G_2 <- 2^N_G_bootcluster
   if (type == "rademacher") {
     if(N_G_2 <= B){
