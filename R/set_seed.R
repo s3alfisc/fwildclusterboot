619002dd12d841911073db553ed3841e48fe815a
Alexander Fischer <alexander-fischer1801@t-online.de>
2022-03-19 14:47:10 +0100

    #33 consolidate warning() for full enumeration case; document get_seed()

--- R/set_seed.R
+++ R/set_seed.R
@@ -41,6 +41,8 @@
 # for using set.seed() for controlling rcpp's seed, see this 
 # blog post http://rorynolan.rbind.io/2018/09/30/rcsetseed/
 get_seed <- function() {
+  
+  #' creates an integer based on the global random seed set via set.seed()
   x <- sample.int(.Machine$integer.max, 1)
-  return(x)
+  x
 }
