343bb5d6a7a6b1b630347353fb2d5a4a5bf400bc
Alexander Fischer <alexander-fischer1801@t-online.de>
2022-04-04 20:58:42 +0200

    delete fweights

--- R/boottest_ivreg.R
+++ R/boottest_ivreg.R
@@ -47,7 +47,6 @@
 #'        when fitting the regression object (e.g. if the cluster variable was not used
 #'        when fitting the regression model).
 #' @param floattype Float64 by default. Other option: Float32. Should floating point numbers in Julia be represented as 32 or 64 bit?
-#' @param fweights Logical. FALSE by default, TRUE for frequency weights.
 #' @param getauxweights Logical. FALSE by default. Whether to save auxilliary weight matrix (v)
 #' @param t_boot Logical. Should bootstrapped t-statistics be returned?
 #' @param maxmatsize NULL by default = no limit. Else numeric scalar to set the maximum size of auxilliary weight matrix (v), in gigabytes
