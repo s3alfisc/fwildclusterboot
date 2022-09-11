7f0f2e54cc6d2aa56d7205035addd5b23489a423
Alexander Fischer <alexander-fischer1801@t-online.de>
2022-09-11 22:59:31 +0200

    update tests

--- tests/testthat/test_multicollinearity.R
+++ tests/testthat/test_multicollinearity.R
@@ -5,16 +5,16 @@ test_that("test multicollinearity", {
   X1 <- rnorm(N)
   X2 <- X1
   Y <- rnorm(N)
-  data <- data.frame(
+  data2 <- data.frame(
     Y = Y,
     X1 = X1,
     X2 = X2,
     clustid = sample(1:10, N, TRUE)
   )
 
-  lm_fit <- lm(Y ~ X1 + X2, data = data)
-  feols_fit <- fixest::feols(Y ~ X1 + X2, data = data)
-  felm_fit <- lfe::felm(Y ~ X1 + X2, data = data)
+  lm_fit <- lm(Y ~ X1 + X2, data = data2)
+  suppressWarnings(feols_fit <- fixest::feols(Y ~ X1 + X2, data = data2))
+  suppressWarnings(felm_fit <- lfe::felm(Y ~ X1 + X2, data = data2))
 
   boot_lm <-
     boottest(
