37c54cc8e694abef6a206381966511d84954d4cd
Alexander Fischer <alexander-fischer1801@t-online.de>
2022-09-18 21:25:31 +0200

    further doc errors

--- R/boot_aggregate.R
+++ R/boot_aggregate.R
@@ -1,3 +1,193 @@
+#' Simple tool that aggregates the value of CATT coefficients in
+#' staggered difference-in-difference setups with inference based on 
+#' a wild cluster bootstrap (see details) - similar to `fixest::aggregate()`
+#' 
+#' This is a function helping to replicate the estimator from Sun and
+#' Abraham (2021, Journal of Econometrics). You first need to perform 
+#' an estimation with cohort and relative periods dummies 
+#' (typically using the function i), this leads to estimators of the 
+#' cohort average treatment effect on the treated (CATT). Then you can
+#' use this function to retrieve the average treatment effect on each 
+#' relative period,or for any other way you wish to aggregate the CATT.
+#' 
+#' Note that contrary to the SA article, here the cohort share 
+#' in the sample is considered to be a perfect measure for the 
+#' cohort share in the population.
+#' 
+#' Most of this function is written by Laurent Bergé and used 
+#' in the fixest package published under GPL-3, 
+#' https://cran.r-project.org/web/packages/fixest/index.html
+#' minor changes by Alexander Fischer
+#' 
+#' @param x An object of type fixest estimated using `sunab()`
+#' @param agg A character scalar describing the variable names to be
+#'  aggregated, it is pattern-based. All variables that match the pattern 
+#'  will be aggregated. It must be of the form `"(root)"`, the parentheses 
+#'  must be there and the resulting variable name will be `"root"`. You 
+#'  can add another root with parentheses: `"(root1)regex(root2)"`, in
+#'  which case the resulting name is `"root1::root2"`. To name the resulting 
+#'  variable differently you can pass a named vector: `c("name" = "pattern")`
+#'  or `c("name" = "pattern(root2)")`. It's a bit intricate sorry, please
+#'  see the examples.
+#' @param full Logical scalar, defaults to `FALSE`. If `TRUE`, then all 
+#' coefficients are returned, not only the aggregated coefficients.
+#' @param use_weights Logical, default is `TRUE`. If the estimation was 
+#' weighted, whether the aggregation should take into account the weights.
+#'  Basically if the weights reflected frequency it should be `TRUE`.
+#' @param clustid A character vector or rhs formula containing the names of the
+#' cluster variables. If NULL,
+#'        a heteroskedasticity-robust (HC1) wild bootstrap is run.
+#' @param B Integer. The number of bootstrap iterations. When the number of
+#'  clusters is low,
+#'        increasing B adds little additional runtime.
+#' @param bootcluster A character vector or rhs formula of length 1. Specifies
+#' the bootstrap clustering variable or variables. If more
+#'        than one variable is specified, then bootstrapping is clustered by the
+#'         intersections of
+#'        clustering implied by the listed variables. To mimic the behavior of
+#'        stata's boottest command,
+#'        the default is to cluster by the intersection of all the variables
+#'        specified via the `clustid` argument,
+#'        even though that is not necessarily recommended (see the paper by
+#'         Roodman et al cited below, section 4.2).
+#'        Other options include "min", where bootstrapping is clustered by
+#'        the cluster variable with the fewest clusters.
+#'        Further, the subcluster bootstrap (MacKinnon & Webb, 2018) is
+#'         supported - see the \code{vignette("fwildclusterboot", package =
+#'          "fwildclusterboot")} for details.
+#' @param fe A character vector or rhs formula of length one which contains
+#' the name of the fixed effect to be projected
+#'        out in the bootstrap. Note: if regression weights are used, fe
+#'        needs to be NULL.
+#' @param sign_level A numeric between 0 and 1 which sets the significance level
+#'        of the inference procedure. E.g. sign_level = 0.05
+#'        returns 0.95% confidence intervals. By default, sign_level = 0.05.
+#' @param engine Character scalar. Either "R", "R-lean" or "WildBootTests.jl".
+#'  Controls if `boottest()` should run via its native R implementation 
+#'  or `WildBootTests.jl`.
+#'  "R" is the default and implements the cluster bootstrap
+#'  as in Roodman (2019). "WildBootTests.jl" executes the
+#'  wild cluster bootstrap via the WildBootTests.jl
+#'  package. For it to run, Julia and WildBootTests.jl need
+#'  to be installed.
+#'  The "R-lean" algorithm is a memory friendly, but less
+#'  performant rcpp-armadillo based implementation of the wild
+#'  cluster bootstrap.
+#'  Note that if no cluster is provided, boottest() always
+#'  defaults to the "lean" algorithm. You can set the employed
+#'  algorithm globally by using the
+#'  `setBoottest_engine()` function.
+#' @param bootstrap_type Determines which wild cluster bootstrap type should be 
+#' run. Options are "fnw11", which runs a "11" type 
+#' wild cluster bootstrap via the algorithm outlined in "fast and wild" 
+#' (Roodman et al (2019)). 
+#' @param seed An integer. Allows to set a random seed. For details, see below.
+#' @param beta0 Deprecated function argument. Replaced by function argument 'r'.
+#' @param type character or function. The character string specifies the type
+#'        of boostrap to use: One of "rademacher", "mammen", "norm"
+#'        and "webb". Alternatively, type can be a function(n) for drawing
+#'        wild bootstrap factors. "rademacher" by default.
+#'        For the Rademacher distribution, if the number of replications B
+#'        exceeds
+#'        the number of possible draw ombinations, 2^(#number of clusters),
+#'         then `boottest()`
+#'        will use each possible combination once (enumeration).
+#' @param impose_null Logical. Controls if the null hypothesis is imposed on
+#'        the bootstrap dgp or not. Null imposed `(WCR)` by default.
+#'        If FALSE, the null is not imposed `(WCU)`
+#' @param p_val_type Character vector of length 1. Type of p-value.
+#'        By default "two-tailed". Other options include "equal-tailed",
+#'        ">" and "<".
+#' @param tol Numeric vector of length 1. The desired accuracy
+#'        (convergence tolerance) used in the root finding procedure to find
+#'         the confidence interval.
+#'        1e-6 by default.
+#' @param maxiter Integer. Maximum number of iterations used in the root
+#' finding procedure to find the confidence interval.
+#'        10 by default.
+#' @param nthreads The number of threads. Can be: a) an integer lower than,
+#'                 or equal to, the maximum number of threads; b) 0: meaning
+#'                 all available threads will be used; c) a number strictly
+#'                 between 0 and 1 which represents the fraction of all threads
+#'                 to use. The default is to use 1 core.
+#' @param ssc An object of class `boot_ssc.type` obtained with the function
+#'  \code{\link[fwildclusterboot]{boot_ssc}}. Represents how the small sample
+#'   adjustments are computed. The defaults are `adj = TRUE, fixef.K = "none",
+#'   cluster.adj = "TRUE", cluster.df = "conventional"`.
+#'             You can find more details in the help file for `boot_ssc()`.
+#'             The function is purposefully designed to mimic fixest's
+#'             \code{\link[fixest]{ssc}} function.
+#' @param getauxweights Logical. Whether to save auxilliary weight matrix (v)
+#' @param floattype Float64 by default. Other option: Float32. Should floating
+#'  point numbers in Julia be represented as 32 or 64 bit? Only relevant when
+#'   'engine = "WildBootTests.jl"'
+#' @param maxmatsize NULL by default = no limit. Else numeric scalar to set
+#' the maximum size of auxilliary weight matrix (v), in gigabytes. Only
+#' relevant when 'engine = "WildBootTests.jl"'
+#' @param bootstrapc Logical scalar, FALSE by default. TRUE  to request
+#' bootstrap-c instead of bootstrap-t. Only relevant when
+#' 'engine = "WildBootTests.jl"'
+#' @param ... misc function arguments 
+#' 
+#' @export
+#' 
+#' @importFrom utils setTxtProgressBar txtProgressBar
+#' 
+#' @return A data frame with aggregated coefficients, p-values and 
+#' confidence intervals. 
+#' 
+#' @importFrom dreamerr check_value_plus
+#' 
+#' @examples 
+#' 
+#' \donttest{
+#' library(fwildclusterboot)
+#' library(fixest)
+#' library(did2s)
+#' data("df_het", package = "did2s")
+#' object = feols(
+#'   dep_var ~ sunab(year - rel_year, year) | state + year, 
+#'   df_het, 
+#'   cluster = ~state
+#' )
+#' 
+#' 
+#' agg1 = boot_aggregate(
+#'   x = object,
+#'   agg = TRUE,
+#'   full = FALSE,
+#'   use_weights = TRUE,
+#'   B = 9999, 
+#'   clustid = ~state,
+#'   nthreads = 1,
+#'   sign_level = 0.05,
+#'   impose_null = TRUE,
+#'   fe = ~ state
+#' )
+#' 
+#' agg1 |> head()
+#' pvalue(summary(object, agg = TRUE)) |> head()
+#' confint(summary(object, agg = TRUE)) |> head()
+#' 
+#' agg1 = fwildclusterboot:::boot_aggregate(
+#'   x = object,
+#'   agg = "att",
+#'   full = FALSE,
+#'   use_weights = TRUE,
+#'   B = 9999, 
+#'   clustid = ~state,
+#'   nthreads = 1,
+#'   sign_level = 0.05,
+#'   impose_null = TRUE,
+#'   fe = ~ state
+#' )
+#' 
+#' agg1 |> head()
+#' pvalue(summary(object, agg = "att")) |> head()
+#' confint(summary(object, agg = "att")) |> head()
+#' }
+
+
 boot_aggregate = function(
     x,
     agg,
@@ -30,198 +220,9 @@ boot_aggregate = function(
     getauxweights = FALSE,
     ...){
   
-  #' Simple tool that aggregates the value of CATT coefficients in
-  #' staggered difference-in-difference setups with inference based on 
-  #' a wild cluster bootstrap (see details) - similar to `fixest::aggregate()`
-  #' 
-  #' This is a function helping to replicate the estimator from Sun and
-  #' Abraham (2021, Journal of Econometrics). You first need to perform 
-  #' an estimation with cohort and relative periods dummies 
-  #' (typically using the function i), this leads to estimators of the 
-  #' cohort average treatment effect on the treated (CATT). Then you can
-  #' use this function to retrieve the average treatment effect on each 
-  #' relative period,or for any other way you wish to aggregate the CATT.
-  #' 
-  #' Note that contrary to the SA article, here the cohort share 
-  #' in the sample is considered to be a perfect measure for the 
-  #' cohort share in the population.
-  #' 
-  #' Most of this function is written by Laurent Bergé and used 
-  #' in the fixest package published under GPL-3, 
-  #' https://cran.r-project.org/web/packages/fixest/index.html
-  #' minor changes by Alexander Fischer
-  #' 
-  #' @param x An object of type fixest estimated using `sunab()`
-  #' @param agg A character scalar describing the variable names to be
-  #'  aggregated, it is pattern-based. All variables that match the pattern 
-  #'  will be aggregated. It must be of the form `"(root)"`, the parentheses 
-  #'  must be there and the resulting variable name will be `"root"`. You 
-  #'  can add another root with parentheses: `"(root1)regex(root2)"`, in
-  #'  which case the resulting name is `"root1::root2"`. To name the resulting 
-  #'  variable differently you can pass a named vector: `c("name" = "pattern")`
-  #'  or `c("name" = "pattern(root2)")`. It's a bit intricate sorry, please
-  #'  see the examples.
-  #' 
-  #' @param full Logical scalar, defaults to `FALSE`. If `TRUE`, then all 
-  #' coefficients are returned, not only the aggregated coefficients.
-  #' @param use_weights Logical, default is `TRUE`. If the estimation was 
-  #' weighted, whether the aggregation should take into account the weights.
-  #'  Basically if the weights reflected frequency it should be `TRUE`.
-  #'  
-  #' @param clustid A character vector or rhs formula containing the names of the
-  #' cluster variables. If NULL,
-  #'        a heteroskedasticity-robust (HC1) wild bootstrap is run.
-  #' @param B Integer. The number of bootstrap iterations. When the number of
-  #'  clusters is low,
-  #'        increasing B adds little additional runtime.
-  #' @param bootcluster A character vector or rhs formula of length 1. Specifies
-  #' the bootstrap clustering variable or variables. If more
-  #'        than one variable is specified, then bootstrapping is clustered by the
-  #'         intersections of
-  #'        clustering implied by the listed variables. To mimic the behavior of
-  #'        stata's boottest command,
-  #'        the default is to cluster by the intersection of all the variables
-  #'        specified via the `clustid` argument,
-  #'        even though that is not necessarily recommended (see the paper by
-  #'         Roodman et al cited below, section 4.2).
-  #'        Other options include "min", where bootstrapping is clustered by
-  #'        the cluster variable with the fewest clusters.
-  #'        Further, the subcluster bootstrap (MacKinnon & Webb, 2018) is
-  #'         supported - see the \code{vignette("fwildclusterboot", package =
-  #'          "fwildclusterboot")} for details.
-  #' @param fe A character vector or rhs formula of length one which contains
-  #' the name of the fixed effect to be projected
-  #'        out in the bootstrap. Note: if regression weights are used, fe
-  #'        needs to be NULL.
-  #' @param sign_level A numeric between 0 and 1 which sets the significance level
-  #'        of the inference procedure. E.g. sign_level = 0.05
-  #'        returns 0.95% confidence intervals. By default, sign_level = 0.05.
-  #' @param engine Character scalar. Either "R", "R-lean" or "WildBootTests.jl".
-  #'  Controls if `boottest()` should run via its native R implementation 
-  #'  or `WildBootTests.jl`.
-  #'  "R" is the default and implements the cluster bootstrap
-  #'  as in Roodman (2019). "WildBootTests.jl" executes the
-  #'  wild cluster bootstrap via the WildBootTests.jl
-  #'  package. For it to run, Julia and WildBootTests.jl need
-  #'  to be installed.
-  #'  The "R-lean" algorithm is a memory friendly, but less
-  #'  performant rcpp-armadillo based implementation of the wild
-  #'  cluster bootstrap.
-  #'  Note that if no cluster is provided, boottest() always
-  #'  defaults to the "lean" algorithm. You can set the employed
-  #'  algorithm globally by using the
-  #'  `setBoottest_engine()` function.
-  #' @param bootstrap_type Determines which wild cluster bootstrap type should be 
-  #' run. Options are "fnw11", which runs a "11" type 
-  #' wild cluster bootstrap via the algorithm outlined in "fast and wild" 
-  #' (Roodman et al (2019)). 
-  #' @param seed An integer. Allows to set a random seed. For details, see below.
-  #' @param beta0 Deprecated function argument. Replaced by function argument 'r'.
-  #' @param type character or function. The character string specifies the type
-  #'        of boostrap to use: One of "rademacher", "mammen", "norm"
-  #'        and "webb". Alternatively, type can be a function(n) for drawing
-  #'        wild bootstrap factors. "rademacher" by default.
-  #'        For the Rademacher distribution, if the number of replications B
-  #'        exceeds
-  #'        the number of possible draw ombinations, 2^(#number of clusters),
-  #'         then `boottest()`
-  #'        will use each possible combination once (enumeration).
-  #' @param impose_null Logical. Controls if the null hypothesis is imposed on
-  #'        the bootstrap dgp or not. Null imposed `(WCR)` by default.
-  #'        If FALSE, the null is not imposed `(WCU)`
-  #' @param p_val_type Character vector of length 1. Type of p-value.
-  #'        By default "two-tailed". Other options include "equal-tailed",
-  #'        ">" and "<".
-  #' @param tol Numeric vector of length 1. The desired accuracy
-  #'        (convergence tolerance) used in the root finding procedure to find
-  #'         the confidence interval.
-  #'        1e-6 by default.
-  #' @param maxiter Integer. Maximum number of iterations used in the root
-  #' finding procedure to find the confidence interval.
-  #'        10 by default.
-  #' @param nthreads The number of threads. Can be: a) an integer lower than,
-  #'                 or equal to, the maximum number of threads; b) 0: meaning
-  #'                 all available threads will be used; c) a number strictly
-  #'                 between 0 and 1 which represents the fraction of all threads
-  #'                 to use. The default is to use 1 core.
-  #' @param ssc An object of class `boot_ssc.type` obtained with the function
-  #'  \code{\link[fwildclusterboot]{boot_ssc}}. Represents how the small sample
-  #'   adjustments are computed. The defaults are `adj = TRUE, fixef.K = "none",
-  #'   cluster.adj = "TRUE", cluster.df = "conventional"`.
-  #'             You can find more details in the help file for `boot_ssc()`.
-  #'             The function is purposefully designed to mimic fixest's
-  #'             \code{\link[fixest]{ssc}} function.
-  #' @param getauxweights Logical. Whether to save auxilliary weight matrix (v)
-  #' @param floattype Float64 by default. Other option: Float32. Should floating
-  #'  point numbers in Julia be represented as 32 or 64 bit? Only relevant when
-  #'   'engine = "WildBootTests.jl"'
-  #' @param maxmatsize NULL by default = no limit. Else numeric scalar to set
-  #' the maximum size of auxilliary weight matrix (v), in gigabytes. Only
-  #' relevant when 'engine = "WildBootTests.jl"'
-  #' @param bootstrapc Logical scalar, FALSE by default. TRUE  to request
-  #' bootstrap-c instead of bootstrap-t. Only relevant when
-  #' 'engine = "WildBootTests.jl"'
-  
-  #' @param ... misc function arguments 
-  #' 
-  #' @export
-  #' 
-  #' @importFrom utils setTxtProgressBar txtProgressBar
-  #' 
-  #' @return A data frame with aggregated coefficients, p-values and 
-  #' confidence intervals. 
-  #' 
-  #' @importFrom dreamerr check_value_plus
-  #' 
-  #' @examples 
-  #' 
-  #' library(fwildclusterboot)
-  #' library(fixest)
-  #' library(did2s)
-  #' data("df_het", package = "did2s")
-  #' object = feols(
-  #'   dep_var ~ sunab(year - rel_year, year) | state + year, 
-  #'   df_het, 
-  #'   cluster = ~state
-  #' )
-  #' 
-  #' #sunab <- fixest:::sunab
-  #' 
-  #' agg1 = fwildclusterboot:::boot_aggregate(
-  #'   x = object,
-  #'   agg = TRUE,
-  #'   full = FALSE,
-  #'   use_weights = TRUE,
-  #'   B = 9999, 
-  #'   clustid = ~state,
-  #'   nthreads = 1,
-  #'   sign_level = 0.05,
-  #'   impose_null = TRUE,
-  #'   fe = ~ state
-  #' )
-  #' 
-  #' agg1 |> head()
-  #' pvalue(summary(object, agg = TRUE)) |> head()
-  #' confint(summary(object, agg = TRUE)) |> head()
-  #' 
-  #' agg1 = fwildclusterboot:::boot_aggregate(
-  #'   x = object,
-  #'   agg = "att",
-  #'   full = FALSE,
-  #'   use_weights = TRUE,
-  #'   B = 9999, 
-  #'   clustid = ~state,
-  #'   nthreads = 1,
-  #'   sign_level = 0.05,
-  #'   impose_null = TRUE,
-  #'   fe = ~ state
-  #' )
-  #' 
-  #' agg1 |> head()
-  #' pvalue(summary(object, agg = "att")) |> head()
-  #' confint(summary(object, agg = "att")) |> head()
 
   # note: all boottest function arguments are tested in boottest()
+  # therefore, only check for supported subset of features
 
   check_arg(bootstrap_type, "charin(fnw11)")
   
@@ -297,7 +298,6 @@ boot_aggregate = function(
   }
 
   mm = model.matrix(x)
-  param = colnames(mm)[1]
 
   cat("Run the wild bootstrap: this might take some time...(but 
       hopefully not too much time =) ).", "\n")
