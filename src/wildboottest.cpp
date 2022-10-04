80aaaa49dadd57c2084f203b6903da16297fcede
Alexander Fischer <alexander-fischer1801@t-online.de>
2022-10-04 14:37:30 +0200

    #63 add error message when type x3 and felm

--- src/wildboottest.cpp
+++ src/wildboottest.cpp
@@ -3,12 +3,12 @@
 #include <RcppEigen.h>
 #ifdef _OPENMP
 #include <omp.h>
-// [[Rcpp::plugins(openmp)]]
 #else
 #define omp_get_max_threads() 0
 #define EIGEN_DONT_PARALLELIZE
 #endif
 
+// [[Rcpp::plugins(openmp)]]
 // [[Rcpp::depends(RcppArmadillo, RcppEigen)]]
 
 using namespace Rcpp;
