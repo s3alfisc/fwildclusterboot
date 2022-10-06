07e4755adb15065e9f79ce4576d797ebad46107d
Alexander Fischer <alexander-fischer1801@t-online.de>
2022-10-06 16:28:59 +0200

    increase relative tolerance in sunab tests to 5%

--- tests/testthat/test_sunab.R
+++ tests/testthat/test_sunab.R
@@ -1,18 +1,17 @@
-test_that("test sunab()", {
+test_that("test sunab", {
 
   skip_on_cran()
-  
   library(fixest)
   # Simple DiD example
-  data(base_stagg)
+  base_stagg <- fixest::base_stagg
 
   # The DiD estimation
-  res_sunab = feols(y ~ x1 + sunab(year_treated, year) | id + year, base_stagg)
+  res_sunab = fixest::feols(y ~ x1 + fixest:::sunab(year_treated, year) | id + year, base_stagg)
   
-  res_sunab_3ref = feols(y ~ x1 + sunab(year_treated, year, ref.p = c(.F + 0:2, -1)) |
+  res_sunab_3ref = fixest::feols(y ~ x1 + fixest:::sunab(year_treated, year, ref.p = c(.F + 0:2, -1)) |
                            id + year, 
                          cluster = "id", base_stagg, 
-                         ssc = ssc(adj = FALSE, cluster.adj = FALSE))
+                         ssc = fixest::ssc(adj = FALSE, cluster.adj = FALSE))
   
   fixest_att <- aggregate(res_sunab, agg = "ATT")
   fixest_ci <- as.matrix(confint(summary(res_sunab, agg = "ATT")))
@@ -61,14 +60,14 @@ test_that("test sunab()", {
     fixest_ci[-1,],
     boot_year[,3:4], 
     ignore_attr = TRUE, 
-    tolerance = 0.033
+    tolerance = 0.05
   )
   
   expect_equal(
     fixest_pval[-1],
     boot_year[,2], 
     ignore_attr = TRUE, 
-    tolerance = 0.04
+    tolerance = 0.05
   )
   
   
